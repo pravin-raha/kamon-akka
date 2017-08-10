7a8ee8ffbb96dfcb906f0c74143c9fb2fe2929fa
pravin <pravinrahangdale41@gmail.com>
2017-08-10 12:56:44 +0530

    initial commit

---
+++ build.sbt
@@ -0,0 +1,16 @@
+name := "kamon-akka"
+
+version := "1.0"
+
+scalaVersion := "2.11.11"
+
+resolvers += Resolver.bintrayRepo("kamon-io", "releases")
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.2",
+  "com.typesafe.akka" %% "akka-slf4j" % "2.5.2",
+  "io.kamon" %% "kamon-core" % "0.6.7",
+  "io.kamon" %% "kamon-akka-2.5" % "0.6.7",
+  "io.kamon" %% "kamon-log-reporter" % "0.6.8",
+  "ch.qos.logback" % "logback-classic" % "1.2.3"
+)
---
+++ src/main/scala/com/example/AutomaticTraceContextPropagationWithFutures.scala
@@ -0,0 +1,39 @@
+package com.example
+
+import akka.actor.{Actor, ActorSystem, Props}
+import kamon.Kamon
+
+/**
+  * Created by synerzip on 9/8/17.
+  */
+object AutomaticTraceContextPropagationWithFutures extends App {
+  Kamon.start()
+
+  val system = ActorSystem()
+
+  val pinger = system.actorOf(Props(new Actor {
+    def receive: Actor.Receive = {
+      case "pong" ⇒ sender ! "ping"
+    }
+  }), "pinger")
+
+  val ponger = system.actorOf(Props(new Actor {
+    def receive: Actor.Receive = {
+      case "ping" ⇒ sender ! "pong"
+    }
+  }), "ponger")
+
+  pinger.tell("pong", ponger)
+
+}
+
+class DumbActor extends Actor {
+  def receive = {
+    case "hello" => {
+      println("world")
+      sender ! "world"
+    }
+    case other =>
+  }
+}
+
